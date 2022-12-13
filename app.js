59673d173f0e3fa5aa740ca2a63c75af42336436
さとうまい <satoumai@satoumainoMacBook-Air.local>
2022-12-14 00:36:22 +0900

    課題提出_2回目

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 for(let i = 1; i<=50; i++)
 
+if(i%4===0){
+  console.log('4で割れる数字です.' + i); 
+}
+
+for(let i = 1; i<=50; i++)
+
 if(i==10){
   console.log('今' + i + '回ループしました。');
 }else if(i==20){
