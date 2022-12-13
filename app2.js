e7c651498f5e24d8f44c5a56e37e5e98feaefa1a
さとうまい <satoumai@satoumainoMacBook-Air.local>
2022-12-13 23:53:00 +0900

    課題提出_1回目

---
+++ app2.js
@@ -0,0 +1,16 @@
+for(let i = 1; i<=50; i++)
+
+if(i==10){
+  console.log('今' + i + '回ループしました。');
+}else if(i==20){
+  console.log('今' + i + '回ループしました。');
+}else if(i==30){
+  console.log('今' + i + '回ループしました。');
+}else if(i==40){
+  console.log('今' + i + '回ループしました。');
+}else if(i==50){
+  console.log('今' + i + '回ループしました。');
+  alert('50回カウント終わりました。');
+}
+
+
